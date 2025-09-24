8d0448f121aa2d6229dad61fb4fa6b49e58ff3cc
divanshu gokharu <you@example.com>
2025-09-24 08:21:19 +0530

    no frontend

--- server.js
+++ server.js
@@ -36,15 +36,15 @@ app.use("/api/medicines", medicineRoutes);
 app.use(errorHandler);
 
 // Serve frontend in production
-if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "../frontend/build");
-  app.use(express.static(frontendPath));
+//if (process.env.NODE_ENV === "production") {
+  //const frontendPath = path.join(__dirname, "../frontend/build");
+  // app.use(express.static(frontendPath));
 
   // Correct fallback route for React
-  app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
-  });
-}
+  //app.get(/.*/, (req, res) => {
+    //res.sendFile(path.join(frontendPath, "index.html"));
+  //});
+//}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
