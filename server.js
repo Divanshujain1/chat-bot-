75f11ca4224bebae032926d4d669c50f512d2ce3
divanshu gokharu <you@example.com>
2025-09-24 08:06:56 +0530

    deployment

--- server.js
+++ server.js
@@ -41,9 +41,10 @@ if (process.env.NODE_ENV === "production") {
   const frontendPath = path.join(__dirname, "../frontend/build");
   app.use(express.static(frontendPath));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
-  });
+ app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 }
 
 const PORT = process.env.PORT || 5000;
